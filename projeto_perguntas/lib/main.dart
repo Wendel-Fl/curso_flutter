852d7f8ffb892e20d19cd4d5804dcacfad780a76
WendelFl <wendelfl.dev@gmail.com>
2023-05-02 16:18:05 -0300

    testando flutter.

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Olá Flutter!!!'),
+    );
+  }
+}
