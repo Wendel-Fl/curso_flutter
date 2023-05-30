6a5351bc6a732bb46fe5e0652c3fe5fb6cd660d9
Wendel-Fl <wendelfl.dev@gmail.com>
2023-05-30 14:51:00 -0300

    :sparkles: Configurando fontes e adicionando imagem para o caso de nenhum transação.

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -48,19 +48,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Sleeper',
-      value: 185.90,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'The Fade Out',
-      value: 147.79,
-      date: DateTime.now(),
-    ),
+  final List<Transaction> _transactions = [
+    // Transaction(
+    //   id: 't1',
+    //   title: 'Sleeper',
+    //   value: 185.90,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't2',
+    //   title: 'The Fade Out',
+    //   value: 147.79,
+    //   date: DateTime.now(),
+    // ),
   ];
 
   _addTransaction(String title, double value) {
