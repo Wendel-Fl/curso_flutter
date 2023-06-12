e69a3fd6bf82658b854ee0eb799dd248847bdf0a
Wendel-Fl <wendelfl.dev@gmail.com>
2023-06-12 10:12:07 -0300

    :art: Adicionando as categorias

--- meals/lib/screens/categories_screen.dart
+++ meals/lib/screens/categories_screen.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import '../components/category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Vamos cozinhar?'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25.0),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
-          crossAxisSpacing: 200,
-          mainAxisSpacing: 20),
-      children: [],
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat);
+        }).toList(),
+      ),
     );
   }
 }
