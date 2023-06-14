c21356371f1c6cbb506185aa8c0facd0daa397c1
Wendel-Fl <wendelfl.dev@gmail.com>
2023-06-14 14:45:52 -0300

    :art: Ícones de preço, custo e duração

--- meals/lib/components/meal_item.dart
+++ meals/lib/components/meal_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/utils/app_routes.dart';
 import '../models/meal.dart';
 
 class MealItem extends StatelessWidget {
@@ -6,9 +7,17 @@ class MealItem extends StatelessWidget {
 
   const MealItem(this.meal, {super.key});
 
+  void _selectMeal(BuildContext context) {
+    Navigator.of(context).pushNamed(
+      AppRoutes.mealDetail,
+      arguments: meal,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: () => _selectMeal(context),
       child: Card(
         margin: const EdgeInsets.all(10.0),
         elevation: 4,
@@ -30,7 +39,54 @@ class MealItem extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5.0,
+                      horizontal: 20.0,
+                    ),
+                    child: Text(
+                      meal.title,
+                      style: const TextStyle(fontSize: 20, color: Colors.white),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
               ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      const Icon(Icons.schedule),
+                      const SizedBox(width: 6),
+                      Text('${meal.duration} min'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.work),
+                      const SizedBox(width: 6),
+                      Text(meal.complexityText),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(Icons.attach_money),
+                      const SizedBox(width: 6),
+                      Text(meal.costText),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
