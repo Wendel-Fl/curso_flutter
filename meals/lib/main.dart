656a644863470f2c0b0fedcd7c8ae82983d6715a
Wendel-Fl <wendelfl.dev@gmail.com>
2023-06-16 14:56:57 -0300

    :truck: Rota de configuração

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -1,5 +1,6 @@
 import 'utils/app_routes.dart';
 import '/screens/tabs_screen.dart';
+import 'screens/settings_screen.dart';
 import 'package:flutter/material.dart';
 import 'screens/meal_detail_screen.dart';
 import 'screens/categories_meals_screen.dart';
@@ -31,6 +32,7 @@ class MyApp extends StatelessWidget {
         AppRoutes.home: (ctx) => const TabsScreen(),
         AppRoutes.categoriesMeals: (ctx) => const CategoriesMealsScreen(),
         AppRoutes.mealDetail: (ctx) => const MealDetailScreen(),
+        AppRoutes.settings: (ctx) => const SettingsScreen(),
       },
     );
   }
