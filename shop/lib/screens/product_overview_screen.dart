c1d0cd0ee1ebcfa7c57809e7cb5742387737a91b
Wendel-Fl <wendelfl.dev@gmail.com>
2023-06-23 13:56:25 -0300

    :construction: Funcionalidade para favoritar produtos em progresso

--- shop/lib/screens/product_overview_screen.dart
+++ shop/lib/screens/product_overview_screen.dart
@@ -1,33 +1,36 @@
 import 'package:flutter/material.dart';
 
-import '../models/product.dart';
-import '../data/dummy_data.dart';
-import '../components/product_item.dart';
+import '../components/product_grid.dart';
+
+enum FilterOption { favorite, all }
 
 class ProductOverviewScreen extends StatelessWidget {
   final String title;
-  final List<Product> loadedProducts = dummyProducts;
 
-  ProductOverviewScreen({required this.title, super.key});
+  const ProductOverviewScreen({required this.title, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: FilterOption.favorite,
+                child: Text('Somente favoritos'),
+              ),
+              const PopupMenuItem(
+                value: FilterOption.all,
+                child: Text('Todos'),
+              ),
+            ],
+            onSelected: (FilterOption selected) => print(selected),
+          ),
+        ],
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10.0),
-        itemCount: dummyProducts.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10),
-        itemBuilder: (ctx, index) => ProductItem(
-          product: loadedProducts[index],
-        ),
-      ),
+      body: const ProductGrid(),
     );
   }
 }
