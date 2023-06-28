5735e3ada1133370758cab4e4235704b3bd2c14c
Wendel-Fl <wendelfl.dev@gmail.com>
2023-06-28 11:58:45 -0300

    :sparkles: Tela do carrinho

---
+++ shop/lib/components/cart_widget.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/cart.dart';
+import '../models/cart_item.dart';
+
+class CartWidget extends StatelessWidget {
+  final CartItem cartItem;
+  const CartWidget(this.cartItem, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      key: ValueKey(cartItem.id),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20.0),
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        color: Theme.of(context).colorScheme.error,
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+      ),
+      onDismissed: (_) {
+        Provider.of<Cart>(
+          context,
+          listen: false,
+        ).removeItem(cartItem.productId);
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: FittedBox(
+                  child: Text('${cartItem.price}'),
+                ),
+              ),
+            ),
+            title: Text(cartItem.name),
+            subtitle: Text('Total: R\$ ${cartItem.price * cartItem.quantity}'),
+            trailing: Text('${cartItem.quantity}x'),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ shop/lib/screens/cart_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/cart_widget.dart';
+import 'package:shop/models/order_list.dart';
+
+import '../models/cart.dart';
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Cart cart = Provider.of(context);
+    final items = cart.items.values.toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Carrinho'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin:
+                const EdgeInsets.symmetric(horizontal: 15.0, vertical: 25.0),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Total',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  const SizedBox(width: 10.0),
+                  Chip(
+                    backgroundColor: Theme.of(context).primaryColor,
+                    label: Text(
+                      'R\$${cart.totalAmount}',
+                      style: TextStyle(
+                        color: Theme.of(context)
+                            .primaryTextTheme
+                            .headlineSmall
+                            ?.color,
+                      ),
+                    ),
+                  ),
+                  const Spacer(),
+                  TextButton(
+                      onPressed: () {
+                        Provider.of<OrderList>(
+                          context,
+                          listen: false,
+                        ).addOrder(cart);
+                        cart.clear();
+                      },
+                      child: const Text('COMPRAR'))
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (ctx, index) => CartWidget(items[index]),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
