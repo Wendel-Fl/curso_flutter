06afa67c7b2f819e2aafad152672d09e3f04385c
Wendel-Fl <wendelfl.dev@gmail.com>
2023-06-29 11:00:57 -0300

    :art: Tela de pedidos

--- shop/lib/screens/orders_screen.dart
+++ shop/lib/screens/orders_screen.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/order_widget.dart';
+
+import '../components/app_drawer.dart';
+import '../models/order_list.dart';
 
 class OrdersScreen extends StatelessWidget {
   const OrdersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final OrderList orders = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Meus Pedidos'),
+      ),
+      drawer: const AppDrawer(),
+      body: ListView.builder(
+        itemCount: orders.itemsCount,
+        itemBuilder: (ctx, index) => OrderWidget(order: orders.items[index]),
       ),
     );
   }
